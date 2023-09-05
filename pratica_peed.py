5e0912c3d07148f004421f7206499837ee6f51d9
naielyvieira <naielyvieira100@gmail.com>
2023-09-05 15:43:49 -0300

    Arvore Binaria

--- pratica_peed.py
+++ pratica_peed.py
@@ -8,85 +8,87 @@ class ArvoreBinaria:
     def __init__(self):
         self.raiz = None
         
-    def insere(self, valor):
+    def inserir(self, valor):
         if self.raiz is None:
             self.raiz = No(valor)
         else:
-            self._insere_recursivamente(self.raiz, valor)
+            self._inserir_recursivamente(self.raiz, valor)
             
-    def _insere_recursivamente(self, no_atual, valor):
+    def _inserir_recursivamente(self, no_atual, valor):
         if valor < no_atual.valor:
             if no_atual.esquerda is None:
                 no_atual.esquerda = No(valor)
             else:
-                self._insere_recursivamente(no_atual.esquerda, valor)
+                self._inserir_recursivamente(no_atual.esquerda, valor)
         
         elif valor > no_atual.valor:
             if no_atual.direita is None:
                 no_atual.direita = No(valor)
             
             else:
-                self._insere_recursivamente(no_atual.direita, valor)
+                self._inserir_recursivamente(no_atual.direita, valor)
                 
-    def busca(self, valor):
-        return self._busca_recursivamente(self.raiz, valor) 
+    def buscar(self, valor):
+        return self._buscar_recursivamente(self.raiz, valor) 
     
-    def _busca_recursivamente(self, no_atual, valor):
+    def _buscar_recursivamente(self, no_atual, valor):
         if no_atual is None:
             return False
         if valor == no_atual.valor:
             return True
         if valor < no_atual.valor:
-            return self._busca_recursivamente(no_atual.esquerda, valor)
-        return self._busca_recursivamente(no_atual.direita, valor)
+            return self._buscar_recursivamente(no_atual.esquerda, valor)
+        return self._buscar_recursivamente(no_atual.direita, valor)
     
     def altura(self):
-        return self._calcula_altura(self.raiz)
+        return self._calcular_altura(self.raiz)
 
-    def _calcula_altura(self, no_atual):
+    def _calcular_altura(self, no_atual):
         if no_atual is None:
             return 0
-        altura_esquerda = self._calcula_altura(no_atual.esquerda)
-        altura_direita = self._calcula_altura(no_atual.direita)
+        altura_esquerda = self._calcular_altura(no_atual.esquerda)
+        altura_direita = self._calcular_altura(no_atual.direita)
         return max(altura_esquerda, altura_direita) + 1
 
     def nos_internos(self):
-        return self._conta_nos_internos(self.raiz)
+        return self._contar_nos_internos(self.raiz)
 
-    def _conta_nos_internos(self, no_atual):
+    def _contar_nos_internos(self, no_atual):
         if no_atual is None:
             return 0
         if no_atual.esquerda or no_atual.direita:
-            return 1 + self._conta_nos_internos(no_atual.esquerda) + self._conta_nos_internos(no_atual.direita)
+            return 1 + self._contar_nos_internos(no_atual.esquerda) + self._contar_nos_internos(no_atual.direita)
         return 0
     def folhas(self):
-        return self._conta_folhas(self.raiz)
+        return self._contar_folhas(self.raiz)
 
-    def _conta_folhas(self, no_atual):
+    def _contar_folhas(self, no_atual):
         if no_atual is None:
             return 0
         if not no_atual.esquerda and not no_atual.direita:
             return 1
-        return self._conta_folhas(no_atual.esquerda) + self._conta_folhas(no_atual.direita)
+        return self._contar_folhas(no_atual.esquerda) + self._contar_folhas(no_atual.direita)
 
 
 # Exemplo de uso:
 if __name__ == "__main__":
     arvore = ArvoreBinaria()
-    numeros = [8, 9, 10, 11 , 12 ,13, 14, 20]
+    numeros = [8, 9, 10, 2, 12 ,13, 14, 20]
 
     for numero in numeros:
-        arvore.insere(numero)
+        arvore.inserir(numero)
 
-    print("Árvore Binária")
-    print("Raiz:", arvore.raiz.valor)
-    print("Altura:", arvore.altura())
-    print("Nós Internos:", arvore.nos_internos())
-    print("Folhas:", arvore.folhas())
+    print("\nÁrvore Binária")
+    print("\nRaiz:", arvore.raiz.valor)
+    print("\nAltura:", arvore.altura())
+    print("\nNós Internos:", arvore.nos_internos())
+    print("\nFolhas:", arvore.folhas())
+    
 
     numero_busca = 4
-    if arvore.busca(numero_busca):
-        print(f"{numero_busca} está presente na árvore.")
+    if arvore.buscar(numero_busca):
+        print(f"\n{numero_busca} está presente na árvore.")
     else:
-        print(f"{numero_busca} não está presente na árvore.")
+        print(f"\n{numero_busca} não está presente na árvore.")
 
+    print("\nNúmeros:", numeros,"\n")
